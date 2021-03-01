0a59fbd6af59fb0dc972928a046c67b879405b3c
Terence-cn <1035260902@qq.com>
2021-03-02 00:14:10 +0800

    problem-47

---
+++ eular-proj/problem-47.py
@@ -0,0 +1,25 @@
+import collections
+
+# 偷来的质因数分解算法
+def fun(num):
+    res, prim = collections.defaultdict(int), 2
+    while num > 1:
+        if num % prim == 0:
+            num //= prim
+            res[prim] += 1
+        else:
+            prim += 1
+    return res
+size=100000000000000000
+i=21
+while i<size:
+    if len(fun(i+3))<4: i+=4
+    elif len(fun(i+2))<4: i+=3
+    elif len(fun(i+1))<4: i+=2
+    elif len(fun(i))<4: i+=1
+    else:
+        print(i,fun(i))
+        print(i+1,fun(i+1))
+        print(i+2,fun(i+2))
+        print(i+3,fun(i+3))
+        break
