a67c3e12d459a1acd319211379d46f80198dc763
TerenceG-cn <terence.guo@foxmail.com>
2021-03-18 19:50:27 +0800

    61

--- eular-proj/problem-61.py
+++ eular-proj/problem-61.py
@@ -1,13 +1,14 @@
 import math
 
+
 # 三边形数
 def is_triangle(n):
-    if math.sqrt(1+8*n)%2==1 :
-        return True
-    return False    
+    return math.sqrt(1 + 8 * n) % 2 == 1
+
+
 # 四边形数
 def is_square(num):
-    low = 1 
+    low = 1
     high = num
     while low < high:
         mid = (low + high) // 2
@@ -19,53 +20,60 @@ def is_square(num):
             high = mid - 1
     return low * low == num
 
+
 # 五边形数
 def is_pentagonal(n):
     # or math.sqrt(1+24*n)%6==1 广义五边形数
-    if math.sqrt(1+24*n)%6==5 :
-        return True
-    return False 
+    return math.sqrt(1 + 24 * n) % 6 == 5
+
+
 # 六边形数
 def is_hexagonal(n):
-    if math.sqrt(1+8*n)%4==1 :
-        return True
-    return False  
+    return math.sqrt(1 + 8 * n) % 4 == 1
+
+
 # 七边形数
 def is_heptagonal(n):
-    if math.sqrt(9+40*n)%10==7 :
-        return True
-    return False  
+    return math.sqrt(9 + 40 * n) % 10 == 7
+
 
 # 八边形数
 def is_octagonal(n):
-    if math.sqrt(1+3*n)%3==2 :
-        return True
-    return False  
-# for i in range(1,100):
-#     n1=i*100+21
-#     n2=7700+i
-#     if (is_octagonal(n1) and is_triangle(n2)) or (is_octagonal(n2) and is_triangle(n1)) :
-#         print(n1,n2)
+    return math.sqrt(1 + 3 * n) % 3 == 2
 
-# for i in range(1111,10000):
-#     if is_triangle(i):
-#         #print("i:",i)
-#         tmp=i%100*100
-#         for j in range(tmp,tmp+101):
-#             if j>999 and is_square(j):
-#                 #print("j:",j)
-#                 tmp=j%100*100
-#                 for k in range(tmp,tmp+101):
-#                     if k>999 and is_pentagonal(k):
-#                         tmp=k%100*100
-#                         for l in range(tmp,tmp+101):
-#                             if l>999 and is_hexagonal(l):
-#                                 print("i j k l:",i,j,k,l)
-#                                 tmp=l%100*100
-#                                 for m in range(tmp,tmp+101):
-#                                     if m>999 and is_heptagonal(m):
-#                                         print("i j k l m:",i,j,k,l,m)
-#                                         tmp=m%100*100
-#                                         for n in range(tmp,tmp+101):
-#                                             if is_octagonal(n):
-#                                                 print(i,j,k,l,m,n)
+
+figurate_numbers_dict = {'triange':[], 'square':[], 'pentagonal':[], 'hexagonal':[], 'heptagonal':[], 'octagonal':[]}
+for num in range(1000, 10000):
+    if is_triangle(num): figurate_numbers_dict['triange'].append(num)
+    if is_square(num): figurate_numbers_dict['square'].append(num)
+    if is_pentagonal(num): figurate_numbers_dict['pentagonal'].append(num)
+    if is_hexagonal(num): figurate_numbers_dict['hexagonal'].append(num)
+    if is_heptagonal(num): figurate_numbers_dict['heptagonal'].append(num)
+    if is_octagonal(num): figurate_numbers_dict['octagonal'].append(num)
+
+def dfs(res_seq,k,count):
+    # print(res_seq,k,count)
+    if count==len(res_seq): 
+        if res_seq[k][0]%100==res_seq['triange'][0]//100:
+            return True
+        else: return False
+    else:
+        for i in res_seq:
+            if res_seq[i][1]==0 and res_seq[k][0]%100==res_seq[i][0]//100:
+                res_seq[i][1]=1
+                return dfs(res_seq,i,count+1)
+        return False    
+        
+for tri in figurate_numbers_dict['triange']:
+    for squ in figurate_numbers_dict['square']:
+        for pen in figurate_numbers_dict['pentagonal']:
+            for hex in figurate_numbers_dict['hexagonal']:
+                for hep in figurate_numbers_dict['heptagonal']:
+                    for oct in figurate_numbers_dict['octagonal']:
+                        res_seq={'triange':[tri,0],'square':[squ,0],'pentagonal':[pen,0],'hexagonal':[hex,0],'heptagonal':[hep,0],'octagonal':[oct,0]}
+                        if dfs(res_seq,k='triange',count=1):
+                            print(res_seq)
+
+
+
+        
